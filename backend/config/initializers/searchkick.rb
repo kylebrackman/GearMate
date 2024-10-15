9a317e0ac573e2955d825e15a98b5b424d518870
Kyle Brackman <kyle.a.brackman@gmail.com>
2024-10-14 19:31:10 -0600

    Update searchkick.rb.

--- backend/config/initializers/searchkick.rb
+++ backend/config/initializers/searchkick.rb
@@ -1,5 +1,16 @@
-Searchkick.client = Elasticsearch::Client.new(
-  hosts: ["http://elasticsearch:9200"], 
-  retry_on_failure: true, 
-  transport_options: { request: { timeout: 250 } }
-)
+# config/initializers/searchkick.rb
+
+Searchkick.client =
+  if Rails.env.production?
+    Elasticsearch::Client.new(
+      url: ENV['BONSAI_URL'], # Use Bonsai Elasticsearch URL in production
+      retry_on_failure: true,
+      transport_options: { request: { timeout: 250 } }
+    )
+  else
+    Elasticsearch::Client.new(
+      hosts: ["http://elasticsearch:9200"], # Local Elasticsearch for development
+      retry_on_failure: true,
+      transport_options: { request: { timeout: 250 } }
+    )
+  end
