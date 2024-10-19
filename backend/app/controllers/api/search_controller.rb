7ce783090a7c5ea57c453635a034ae6b395a7a8d
Kyle Brackman <kyle.a.brackman@gmail.com>
2024-10-19 12:12:05 -0600

    Fix handlesubmit function to submit rental request.

--- backend/app/controllers/api/search_controller.rb
+++ backend/app/controllers/api/search_controller.rb
@@ -3,12 +3,17 @@ class Api::SearchController < ApplicationController
     def item_search
         Rails.logger.info "Item search params: #{params}"
 
-        @name_match = Item.search(params[:name])
-        @location_match = Item.search(params[:location])
+        # @name_match = Item.search(params[:name])
+        # @location_match = Item.search(params[:location])
 
-        
+        # searching just by name below
+        # @results = Item.search(params[:name])
+
+        # searching just by location
+        @results= Item.search(params[:location], params[:name])
         
         Rails.logger.info "Search results: #{@results.inspect}"
+
         render json: @results
     end
 
--- client/src/pages/item/ItemSummary.tsx
+++ client/src/pages/item/ItemSummary.tsx
@@ -308,7 +308,7 @@ const ItemSummary = () => {
                   <Button
                     variant="contained"
                     sx={{ backgroundColor: requestButtonColor }}
-                    onClick={() => void handleSubmit}
+                    onClick={() => void handleSubmit()}
                   >
                     Request
                   </Button>
